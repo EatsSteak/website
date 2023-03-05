ea8c4a141a5f2dde2901de338681ec891ec5ddd5
EatsSteak <79487166+EatsSteak@users.noreply.github.com>
2023-03-05 14:37:33 -0500

    Add files via upload

---
+++ output.lua
@@ -0,0 +1,97 @@
+task.spawn(function()
+    local Remotes = game:GetService("ReplicatedStorage").Remotes
+
+    local Cheat = {
+        Remotes:WaitForChild("clientkick"),
+        Remotes.ClanRemotes:WaitForChild("KickPlayer"),
+        Remotes.ClanRemotes:WaitForChild("LocalKick"),
+    }
+
+    do
+        for i, v in next, Cheat do
+            if v ~= nil then
+                v:Destroy()
+            end
+        end
+    end
+end)
+
+task.spawn(function()
+    local Connection;
+    local Force;
+    
+    local function GetMass(Model)
+        local Mass = 0;
+        
+        for i,v in next,Model:GetDescendants() do
+            if v:IsA("BasePart") then
+                Mass = Mass + v:GetMass();
+            end
+        end
+        
+        return Mass;
+    end
+    
+    local function Float(Character)
+        if Connection then
+            Connection:Disconnect();
+            Connection = nil;
+        end
+        
+        if not Force then
+            Force = Instance.new("BodyForce");
+        end
+        
+        local Root = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart");
+        
+        Force.Parent = Root;
+        
+        Connection = game.RunService.Stepped:Connect(function()
+            if (game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Position - game:GetService("Workspace").FountainKnife.Position).Magnitude > 300 then
+                Root.Velocity = Vector3.new(Root.Velocity.X, 0, Root.Velocity.Z);
+                Force.Force = Vector3.new(0, GetMass(Character) * game.Workspace.Gravity, 0);
+            else
+                Force.Force = Vector3.zero;
+            end
+        end)
+    end
+    
+    if game.Players.LocalPlayer.Character then
+        Float(game.Players.LocalPlayer.Character);
+    end
+    
+    game.Players.LocalPlayer.CharacterAdded:Connect(Float);
+end)
+
+game:GetService("RunService").RenderStepped:connect(function()
+    if game.Players.LocalPlayer.PlayerGui.ScreenGui.UI.Target.Visible == true then
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace[game.Players.LocalPlayer.PlayerGui.ScreenGui.UI.Target.TargetText.Text].HumanoidRootPart.CFrame * CFrame.new(-2, -1.5, 0)
+        game.Players.LocalPlayer.Character['Left Arm']:Destroy()
+        game.Players.LocalPlayer.Character['Left Leg']:Destroy()
+        game.Players.LocalPlayer.Character["Right Leg"]:Destroy()
+		game.Players.LocalPlayer.Character["Right Arm"]:Destroy()
+        game.Workspace.GameMap:Destroy()
+    end
+end)
+
+
+local Player = game.Players.LocalPlayer;
+
+local cooldown = false
+
+task.spawn(function()
+    game:GetService("RunService").Stepped:connect(function()
+        if Player.Character and not cooldown and game.Players.LocalPlayer.PlayerGui.ScreenGui.UI.Target.Visible == true then
+            if Player:DistanceFromCharacter(game.Workspace[game.Players.LocalPlayer.PlayerGui.ScreenGui.UI.Target.TargetText.Text].Head.Position) <= 6.5 and not game.Workspace:FindFirstChild(game.Players.LocalPlayer.PlayerGui.ScreenGui.UI.Target.TargetText.Text).ForceField then
+                Player.PlayerScripts.localknifehandler.HitCheck:Fire(game.Workspace[game.Players.LocalPlayer.PlayerGui.ScreenGui.UI.Target.TargetText.Text])
+                coroutine.wrap(function()
+                    cooldown = true
+                    task.wait(0.75)
+                    cooldown = false
+                end)()
+            else
+                task.wait()
+            end
+        end
+    end)
+end)
